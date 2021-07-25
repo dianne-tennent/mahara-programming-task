d06033ca1b19d756514336aab34239c004f8e780
Dianne-Tennent <diannerosemay@gmail.com>
2021-07-25 21:06:21 +1200

    use csv module to parse data into a js object

--- user_upload.js
+++ user_upload.js
@@ -1,45 +1,46 @@
 #!/usr/bin/env node
-const readline = require('readline');
-const commands = require('./db')
-
-//minimist dependency turns argv into an object
-//node example/parse.js -a beep -b boop
-//{ _: [], a: 'beep', b: 'boop' }
-
-// node example/parse.js -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
-// { _: [ 'foo', 'bar', 'baz' ],
-//   x: 3,
-//   y: 4,
-//   n: 5,
-//   a: true,
-//   b: true,
-//   c: true,
-//   beep: 'boop' }
-
-//slice removes the first two args node and filename
+const csv = require('csv');
+const fs = require('fs')
+//command line directives
 const userInputs = require('minimist')(process.argv.slice(2))
-console.log("userInputs: ", userInputs)
+//console.log("userInputs: ", userInputs)
+
+//create empy array to hold csv file data
+const csvData = {name: [], surname: [], email: []};
+
+fs.createReadStream(__dirname + '/' + userInputs.file)
+    .pipe(
+      csv.parse({
+      delimiter: ','
+      })
+    )
+    .on('data', function (dataRow) {
+
+      csvData.name.push(dataRow[0])
+      csvData.surname.push(dataRow[1])
+      csvData.email.push(dataRow[2])
+    })
+    .on('end', function () {
+      console.log(csvData)
+    })
 
 //acccess first item of argv array
 const cmd = userInputs._[0]
 
-//command line directives to handle
-// userInputs.file = name of CSV to be parsed
-// userInputs.create_table = causes the 'users' table to be built
-// userInputs.dry_run = used with --file directive, to run script but not insert into DB. All other functions executed, but DB won't be altered
-// userInputs.u = username
-// userInputs.p = password
-// userInputs.h = host
-// userInputs.help = prints out list of directives with details
+
 
 //handle command line directives
 if(userInputs.hasOwnProperty('help')) {
+  console.log("help info")
   //print help info
 } else if (userInputs.hasOwnProperty('file') && userInputs.hasOwnProperty('dry_run')) {
   //run without altering db
+  console.log("run without altering db, filename is ", userInputs.file)
 } else {
   //run full script
-  commands.createTable()
+  console.log("Running full script, filename is ", userInputs.file)
+  console.log(userInputs)
+  //commands.createTable()
 }
 
 ////functions required:
@@ -47,18 +48,4 @@ if(userInputs.hasOwnProperty('help')) {
 
 // processData() - take csv input, convert to format able to be inserted into db i.e. list of objects with key-value pairs
 
-
 // insertData() - take list of objects and insert into db
-
-
-switch (cmd) {
-  case 'test':
-    console.log('you typed "test"')
-    break
-  case 'upload':
-
-    console.log('The filename is ', userInputs.file)
-    break
-  default:
-    console.log(`I don't understand that command: ${cmd}`)
-}
